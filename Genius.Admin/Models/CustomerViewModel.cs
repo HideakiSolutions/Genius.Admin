5ed9037bf5366486023a8d05239060a3800903b6
Márcio Hideaki Müller Maebara <marciohideaki@gmail.com>
2024-03-05 19:21:07 -0300

    Regular commit

--- Genius.Admin/Models/CustomerViewModel.cs
+++ Genius.Admin/Models/CustomerViewModel.cs
@@ -3,12 +3,20 @@
     public class CustomerViewModel
     {
         public string id { get; set; }
+
+        public string firstName { get; set; }
+        public string lastName { get; set; }
+        public string fullName { get; set; }
+        public string legalName { get; set; }
+        public string cnaeName { get; set; }
+        public string cpfCnpj { get; set; }
+        public string tradingName { get; set; }
+
         public string email { get; set; }
         public string phoneNumber { get; set; }
-        public string lastName { get; set; }
-        public string firstName { get; set; }
         public string status { get; set; }
         public string externalId { get; set; }
         public string kycStatus { get; set; }
+        public string customerType { get; set; }
     }
 }
--- Genius.Admin/wwwroot/assets/js/pages/customer-detail.js
+++ Genius.Admin/wwwroot/assets/js/pages/customer-detail.js
@@ -1,30 +1,40 @@
 ﻿document.getElementById("select-sellbalance").addEventListener("change", function (event) {
     console.log(event);
     document.getElementById("sell-balance").value = event.currentTarget[event.currentTarget.selectedIndex].value;
+    getBook("SELL", event.currentTarget[event.currentTarget.selectedIndex].text, event.currentTarget[event.currentTarget.selectedIndex].value);
     getQuote("SELL", event.currentTarget[event.currentTarget.selectedIndex].text, event.currentTarget[event.currentTarget.selectedIndex].value);
-    getQuotes("SELL", event.currentTarget[event.currentTarget.selectedIndex].text, event.currentTarget[event.currentTarget.selectedIndex].value);
 });
 
 document.getElementById("select-buybalance").addEventListener("change", function (event) {
     console.log(event);
-    document.getElementById("sell-balance").value = event.currentTarget[event.currentTarget.selectedIndex].value;
+    document.getElementById("buy-balance").value = event.currentTarget[event.currentTarget.selectedIndex].value;
+    getBook("BUY", event.currentTarget[event.currentTarget.selectedIndex].text, event.currentTarget[event.currentTarget.selectedIndex].value);
     getQuote("BUY", event.currentTarget[event.currentTarget.selectedIndex].text, event.currentTarget[event.currentTarget.selectedIndex].value);
-    getQuotes("BUY", event.currentTarget[event.currentTarget.selectedIndex].text, event.currentTarget[event.currentTarget.selectedIndex].value);
 });
 
 function handle(balance) {
     console.log(balance);
 }
 
-function getQuote(side, product, quantity)
+function getBook(side, product, quantity)
 {
     var url = "/Quote/Quotation";
     $.get(url, { product: product }, function (quote)
     {
-        if (quote == null) {
+        if (quote == null)
+        {
+            var row = $("#buy-side-table tr:last-child").clone(true);
+            $("#buy-price").val("0.00");
+            $("#buy-side-table tr").not($("#buy-side-table")).remove();
+            $("td", row).eq(0).html("<div class='text-end'><h5 class='font-size-14 text-muted mb-0'>0.00000000</h5 ><p class='text-muted mb-0 font-size-12'>0</p></div>");
+
+            row = $("#sell-side-table tr:last-child").clone(true);
             $("#sell-price").val("0.00");
+            $("#sell-side-table tr").not($("#sell-side-table")).remove();
+            $("td", row).eq(0).html("<div class='text-end'><h5 class='font-size-14 text-muted mb-0'>0.00000000</h5 ><p class='text-muted mb-0 font-size-12'>0</p></div>");
         }
-        else {
+        else
+        {
             var totalAmount = quote.asks[0][0] * quantity;
             if (side == "SELL") {
                 $("#sell-price").val(quote.asks[0][0]);
@@ -32,18 +42,64 @@ function getQuote(side, product, quantity)
             } else {
                 $("#buy-price").val(quote.asks[0][0]);
                 $("#buy-total-amount").val(totalAmount);
+
             }
 
+            var model = quote;
+
+            //var row = $("#buy-side-table tr:last-child").removeAttr("style").clone(true);
+            if (model.bids.length > 0) {
+                var row = $("#buy-side-table tr:last-child").clone(true);
+                $("#buy-side-table tr").not($("#buy-side-table")).remove();
+                $.each(model.bids, function () {
+                    var quote = this;
+                    $("td", row).eq(0).html("<div class='text-end'><h5 class='font-size-14 text-muted mb-0'>" + quote[0] + "</h5 ><p class='text-muted mb-0 font-size-12'>" + quote[1] + "</p></div>");
+                    //$("td", row).eq(1).html(quote[0]);
+                    $("#buy-side-table").append(row);
+                    row = $("#buy-side-table tr:last-child").clone(true);
+                });
+            }
+            else
+            {
+                var row = $("#buy-side-table tr:last-child").clone(true);
+                $("#buy-side-table tr").not($("#buy-side-table")).remove();
+                $("td", row).eq(0).html("<div class='text-end'><h5 class='font-size-14 text-muted mb-0'>" + quote[0] + "</h5 ><p class='text-muted mb-0 font-size-12'>" + quote[1] + "</p></div>");
+                $("#buy-side-table").append(row);
+            }
+
+            if (model.asks.length > 0) {
+                var row = $("#sell-side-table tr:last-child").removeAttr("style").clone(true);
+                $("#sell-side-table tr").not($("#sell-side-table")).remove();
+                $.each(model.asks, function () {
+                    var quote = this;
+                    //$("td", row).eq(0).html(quote[0]);
+                    $("td", row).eq(0).html("<div class='text-end'><h5 class='font-size-14 text-muted mb-0'>" + quote[0] + "</h5 ><p class='text-muted mb-0 font-size-12'>" + quote[1] + "</p></div>");
+                    $("#sell-side-table").append(row);
+                    row = $("#sell-side-table tr:last-child").clone(true);
+                });
+            }
+            else {
+                var row = $("#sell-side-table tr:last-child").clone(true);
+                $("#sell-side-table tr").not($("#sell-side-table")).remove();
+                $("td", row).eq(0).html("<div class='text-end'><h5 class='font-size-14 text-muted mb-0'>0.00000000</h5 ><p class='text-muted mb-0 font-size-12'>0</p></div>");
+            }
 
         }
     });
 }
 
-function GetQuotes(side, product, qtty) {
+function getQuote(side, product, qtty) {
+    //var request = new { productId = product, size = 10, side = side };
+    var request = '{'
+           + '"productId" : "' + product + '", '
+           + '"size" : "' + qtty + '", '
+           + '"side" : "' + side + '"'
+           + '}';
+
     $.ajax({
-        type: "GET",
-        url: "/Quote/Quotation",
-        data: '{ product: ' + product + '}',
+        type: "POST",
+        url: "https://sandbox.geniusbit.io/orders/askQuote",
+        data: request,
         contentType: "application/json; charset=utf-8",
         dataType: "json",
         success: OnSuccess,
